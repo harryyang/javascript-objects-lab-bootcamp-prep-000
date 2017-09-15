653defc40174b304584331e691a01dad90709273
Harold Yang <harold.h.yang@gmail.com>
2017-09-15 18:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {};
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newRecipes = Object.assign ({}, object, {[key]:value} )
-  return newRecipes
+  var newRecipes = Object.assign ({}, object, {[key]:value} );
+  return newRecipes;
 }
